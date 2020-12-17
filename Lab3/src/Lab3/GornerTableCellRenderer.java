468f22efc28068d27900b2aad98bfea34ba6e69c
Manager <bulynko.sergey.y@gmail.com>
2020-12-17 21:44:05 +0300

    Добавил класс для подсчёта значений

---
+++ Lab3/src/Lab3/GornerTableCellRenderer.java
@@ -0,0 +1,82 @@
+package Lab3;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.FlowLayout;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
+
+public class GornerTableCellRenderer implements TableCellRenderer
+{
+    private JPanel panel = new JPanel();
+    private JLabel label = new JLabel();
+
+    private String needle = null;
+    private DecimalFormat formatter = (DecimalFormat)NumberFormat.getInstance();
+    public GornerTableCellRenderer()
+    {
+// Показывать только 5 знаков после запятой
+// Не использовать группировку (т.е. не отделять тысячи
+// ни запятыми, ни пробелами), т.е. показывать число как "1000",
+// а не "1 000" или "1,000"
+        formatter.setGroupingUsed(false);
+        DecimalFormatSymbols dottedDouble = formatter.getDecimalFormatSymbols();
+        dottedDouble.setDecimalSeparator('.');
+        formatter.setDecimalFormatSymbols(dottedDouble);
+
+// Разместить надпись внутри панели
+        panel.add(label);
+// Установить выравнивание надписи по левому краю панели
+        panel.setLayout(new FlowLayout(FlowLayout.LEFT));
+    }
+
+    public Component getTableCellRendererComponent(JTable table, Object
+            value, boolean isSelected, boolean hasFocus, int row, int col)
+    {
+// Создать компонент панели
+        JPanel panel = new JPanel();
+// Создать компонент надписи
+        JLabel label = new JLabel(value.toString());
+// Разместить надпись внутри панели
+        panel.add(label);
+// Установить выравнивание надписи по левому краю панели
+        panel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        String formattedDouble = formatter.format(value);
+        label.setText(formattedDouble);
+
+        if (col == 0 && needle!=null && needle.equals(formattedDouble))
+        {
+            panel.setBackground(Color.RED);
+        }
+        else
+        {
+            panel.setBackground(Color.WHITE);
+        }
+
+        if (col == 1)
+        {
+            double temp = Double.valueOf(formattedDouble);
+            temp *= Math.pow(10, formatter.getMaximumFractionDigits());
+            int sum = 0;
+            for (int i = 1; i <= formatter.getMaximumFractionDigits(); i++)
+            {
+                sum += temp % (Math.pow(10, i)) / (Math.pow(10, i - 1));
+            }
+            if (sum % 10 == 0 && sum != 0)
+                panel.setBackground(Color.YELLOW);
+        }
+        return panel;
+
+    }
+
+    public void setNeedle(String needle)
+    {
+        this.needle = needle;
+    }
+}
+
